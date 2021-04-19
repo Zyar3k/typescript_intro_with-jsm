5d1841d8e0a7ff28d29f28cd552f594831986ac4
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-19 14:17:37 +0200

    TS interface

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 console.log("Hello Typescript");
+var user = {
+    name: "John",
+    id: 4
+};
+"open" | "closed" | "minimized";
+"locked" | "unlocked";
+1 | 3 | 5 | 7 | 9;
+var odd = 5;
+var getLength = function (param) {
+    return param.length;
+};
+getLength("test"); // 4
+getLength(["test", "test1"]); // 2
+// getLength(4); // Argument of type 'number' is not assignable to parameter of type 'string | string[]'
+// Typescript in cra
+// npx create-react-app ./ --template typescript
+/* ********************************************************************************** */
 // Implicit types
 var helloWorld = "Hello, world";
 // helloWorld = 5; // Type 'number' is not assignable to type 'string'
--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
 console.log("Hello Typescript");
+// Interface
+interface User {
+  name: string;
+  id: number;
+}
+
+const user: User = {
+  name: "John",
+  id: 4,
+  // age: 25,  Type '{ name: string; id: number; age: number; }' is not assignable to type 'User'.
+  // Object literal may only specify known properties, and 'age' does not exist in type 'User'
+};
+
+// Composing types => Union
+type WindowStates = "open" | "closed" | "minimized";
+
+// const windowState: WindowStates = "I dont know "; // Type '"I dont know "' is not assignable to type 'WindowStates'
+
+type LockStates = "locked" | "unlocked";
+type OddNumberUnderTen = 1 | 3 | 5 | 7 | 9;
+
+const odd: OddNumberUnderTen = 5;
+
+const getLength = (param: string | string[]) => {
+  return param.length;
+};
+
+getLength("test"); // 4
+getLength(["test", "test1"]); // 2
+
+// getLength(4); // Argument of type 'number' is not assignable to parameter of type 'string | string[]'
+
+// Typescript in cra
+// npx create-react-app ./ --template typescript
+
+/* ********************************************************************************** */
 
 // Implicit types
 let helloWorld = "Hello, world";
