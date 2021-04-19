44ee2ceb826188d28dd3d90cd118770281c012a1
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-19 13:52:23 +0200

    Types in ts

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Hello Typescript");
+// Implicit types
+var helloWorld = "Hello, world";
+// helloWorld = 5; // Type 'number' is not assignable to type 'string'
+// Explicit types
+var firstName = "Tom";
+var age = 5;
+var x = ["Tom", 5];
+// let y: stringAndNumber = [5, "Tom"]; // Type 'string' is not assignable to type 'number'
+// let z: stringAndNumber = ["Tom", "Tom"]; //Type 'string' is not assignable to type 'number'
+// Enums
+var Continents;
+(function (Continents) {
+    Continents[Continents["NorthAmerica"] = 0] = "NorthAmerica";
+    Continents[Continents["SouthAmerica"] = 1] = "SouthAmerica";
+    Continents[Continents["Africa"] = 2] = "Africa";
+    Continents[Continents["Asia"] = 3] = "Asia";
+    Continents[Continents["Europe"] = 4] = "Europe";
+    Continents[Continents["Antarctica"] = 5] = "Antarctica";
+    Continents[Continents["Australia"] = 6] = "Australia";
+})(Continents || (Continents = {}));
+// usage
+var region = Continents.Europe; // 4
--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
 console.log("Hello Typescript");
+
+// Implicit types
+let helloWorld = "Hello, world";
+
+// helloWorld = 5; // Type 'number' is not assignable to type 'string'
+
+// Explicit types
+let firstName: string = "Tom";
+let age: number = 5;
+
+// firstName = 5; // Type 'number' is not assignable to type 'string'
+// age = "Tom"; // Type 'string' is not assignable to type 'number'
+
+// Built-in Typescript
+// Boolean
+// Number
+// String
+// Array
+// Tuple
+// Enum
+// Unknown
+// Any
+// Void
+// Null and Undefined
+
+// Tuple
+type stringAndNumber = [string, number];
+
+let x: stringAndNumber = ["Tom", 5];
+// let y: stringAndNumber = [5, "Tom"]; // Type 'string' is not assignable to type 'number'
+// let z: stringAndNumber = ["Tom", "Tom"]; //Type 'string' is not assignable to type 'number'
+
+// Enums
+enum Continents {
+  NorthAmerica, // 0
+  SouthAmerica, // 1
+  Africa, // 2
+  Asia,
+  Europe,
+  Antarctica,
+  Australia,
+}
+
+// usage
+const region = Continents.Europe; // 4
